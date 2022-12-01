23683adf1b9b972e99bf9401a86e4d408a4b84ad
hilmy057 <naufalh702@gmail.com>
2022-12-01 18:02:55 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stack_Using_Linked_List
+{
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i ;
+            next = n ;
+        }
+    }
+
+    class Stacks
+    {
+        Node top; 
+
+        public Stacks()
+        {
+            top = null; 
+        }
+        bool empty ()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false); 
+        }
+        public void push (int element )
+        { }
+    }
+}
+    
